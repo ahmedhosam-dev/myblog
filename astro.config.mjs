e880f7acd05d6c43d9f6d9a7284190c80f67cd10
ahmedhosam-dev <ahmed0114702@gmail.com>
2025-08-09 06:10:13 +0300

    some updates for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ahmedhosam.netlify.app',
-	integrations: [mdx(), sitemap()],
-	vite: {
+  site: 'https://ahmedhosam.netlify.app',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
     	plugins: [tailwindcss()],
-  	},
+  },
+
+  adapter: netlify(),
 });
