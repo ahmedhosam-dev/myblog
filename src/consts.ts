ae7677b075d323945a4c115bfaa3a489373d90a6
ahmedhosam-dev <ahmed0114702@gmail.com>
2025-08-09 06:18:19 +0300

    change site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ahmed Hosam';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Welcome to my space, where I share everything about my programming journey. I hope you enjoy your time here and find something useful for you along the way.';
