64864e1bb47d41b74333e52e85c9b4d522ee4130
ahmedhosam-dev <ahmed0114702@gmail.com>
2025-08-11 16:57:53 +0300

    rename sitemap.xml to origin name

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,7 +2,7 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({ site }) => {
-    const sitemapUrl = new URL("/sitemap.xml", site).href;
+    const sitemapUrl = new URL("/sitemap-index.xml", site).href;
     return new Response(`User-agent: *
 Allow: /
 Sitemap: ${sitemapUrl}
